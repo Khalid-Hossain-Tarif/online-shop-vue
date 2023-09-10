f376bb1f53f08e6148764090195670fcac821236
khalid-jatri <khalidhossain.jatri@gmail.com>
2023-09-10 18:07:48 +0600

    product details page design updated

--- src/components/ProductDetails.vue
+++ src/components/ProductDetails.vue
@@ -15,14 +15,25 @@ const singleProduct = computed(() => {
 <template>
   {{ singleProduct }}
 
-<!--  <button @click="router.push({name: 'catalog'})">Back</button>-->
-<!--  <button @click="router.back()">Back</button>-->
+<!--  <button @click="router.push({name: 'catalog'})" class="inline-block bg-blue-600 px-5 py-1 mb-2 rounded text-sm text-white">Back</button>-->
+<!--  <button @click="router.back()" class="inline-block bg-blue-600 px-5 py-1 mb-2 rounded text-sm text-white">Back</button>-->
+<!--  <router-link :to="{name: 'catalog'}" class="inline-block bg-blue-600 px-5 py-1 mb-2 rounded text-sm text-white">Back</router-link>-->
   <router-link to="/" class="inline-block bg-blue-600 px-5 py-1 mb-2 rounded text-sm text-white">Back</router-link>
 
-  <div class="flex">
-    <img :src="singleProduct?.thumbnail" alt="" class="w-1/3">
-    <div class="">
-      <h1 class="text-4xl font-bold p-10">{{ singleProduct?.title }}</h1>
+  <div class="flex gap-10 p-10">
+    <div class="w-1/3 bg-gray-100 p-8">
+      <img :src="singleProduct?.thumbnail" alt="" class="w-full h-96 border border-gray-200">
+      <div class="mt-4 pt-4 border-t border-gray-200 flex justify-between flex-wrap gap-4">
+        <img :src="image" v-for="(image, index) in singleProduct?.images" :key="index" alt="" class="w-24 h-24 border border-gray-200">
+      </div>
+    </div>
+    <div>
+      <h1 class="mb-5 text-5xl font-bold capitalize">{{ singleProduct?.title }}</h1>
+      <p><span class="font-medium">Brand: </span> {{ singleProduct?.brand }}</p>
+      <p><span class="font-medium">Price: </span> {{ singleProduct?.price }}</p>
+      <p><span class="font-medium">Discount: </span> {{ singleProduct?.discountPercentage }}%</p>
+      <p><span class="font-medium">Stock: </span> {{ singleProduct?.stock }}</p>
+      <p><span class="font-medium">Category: </span> {{ singleProduct?.category }}</p>
     </div>
   </div>
 </template>
